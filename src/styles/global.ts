60125a8df07de5673cea3edcc1011153a62ba093
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-03-03 20:36:47 -0300

    Add project theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   *, *::before, *::after {
     margin: 0;
     padding: 0;
@@ -13,11 +25,22 @@ const GlobalStyle = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background: var(--background);
+    color: var(--white);
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubunto, Cantarell, "Open Sans", 'Helvetica Neue', sans-serif;
+  }
+
+  p {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `
 export default GlobalStyle
