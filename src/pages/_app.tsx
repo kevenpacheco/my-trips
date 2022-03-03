3c52ffe41ee6a169e15464bf8a91b735505311de
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-03-03 20:28:36 -0300

    Change project name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>World Map</title>
         <link rel="shortcut icon" href="/image/icon-512.png" />
         <link rel="apple-touch-icon" href="/image/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project showing various places in the world and their curiosities."
         />
       </Head>
       <GlobalStyles />
