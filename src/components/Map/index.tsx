1f1fa53237b2401720bd774598c95185376d090b
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-04-15 19:25:52 -0300

    Add min zoom on map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -44,6 +44,7 @@ const Map = ({ places }: MapProps) => {
         center={[0, 0]}
         zoom={3}
         style={{ width: '100%', height: '100%' }}
+        minZoom={3}
       >
         <CustomTileLayer />
 
